04531f3eb8bcc14e6b7976dbf3a26e752737f011
Sherri Booher <sherribooher74@gmail.com>
2019-07-18 20:13:25 -0400

    Add lodash.js file

---
+++ lodash.js
@@ -0,0 +1,15 @@
+var lodash = require('lodash');
+
+var collection = [74, 17, 69, 2004, 7, 2, 22, 9, 25];
+
+// make use of lodash shuffle function
+// console.log(lodash.shuffle(collection));
+
+// make use of lodash mean function
+// console.log(lodash.mean(collection));
+
+// make use of lodash max function
+//console.log(lodash.max(collection));
+
+// make use of lodash random function
+console.log(lodash.random(0, 89));
